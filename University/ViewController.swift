e10a2e5f0d93c0ad9c4588133f58973c018e155b
alex <sashaputsikwork@gmail.com>
2020-09-04 10:21:11 +0300

    Initial Commit

---
+++ University/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  University
+//
+//  Created by Sasha Putsikovich on 04.09.2020.
+//  Copyright © 2020 Sasha Putsikovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
